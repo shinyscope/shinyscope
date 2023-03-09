62014a217bbb76f16532ef014c2d01ac3f5ed73b
MV <MV@MacBook-Pro-6.local>
2023-03-08 19:29:42 -0800

    Added generic regex code to AssignmentTable.R to get categories for assignments, rename assignments, kept a line of code for "raw points" to filter each assignment.

--- R/HelperScripts/AssignmentTable.R
+++ R/HelperScripts/AssignmentTable.R
@@ -1,24 +1,11 @@
 createAssignTable <- function(data){
   gs_cols <- names(data)
-  assignments <- data.frame(gs_col = gs_cols) %>%
-    mutate(students_col = tolower(gs_col), # make all names lowercase
-           students_col = str_replace_all(students_col, "[[:punct:]]", ""), # remove all punctuation
-           students_col = str_replace_all(students_col, "\\s+", "_"), # replace white space with underscore
-           students_col = str_replace_all(students_col, "ps_", "problem_set_"), # standardize names of ps
-           students_col = str_replace_all(students_col, "rg_", "rq_"), # standardize names of rq
-           students_col = str_replace(students_col, "_max_points", "__max_points"), # differentiate col types
-           students_col = str_replace(students_col, "_submission_time", "__submission_time"),
-           students_col = str_replace(students_col, "_lateness_hms", "__lateness_hms")) %>%
-    mutate(category = case_when(str_detect(students_col, "lab") ~ "lab", # create category variable
-                                str_detect(students_col, "problem") ~ "ps",
-                                str_detect(students_col, "rq") ~ "rq",
-                                str_detect(students_col, "quiz") ~ "quiz",
-                                TRUE ~ "other")) %>%
-    #organized by type, this should apply to gradescope assignments overall
-    mutate(type = case_when(!str_detect(students_col, "name|sections|max|time|late")~ "raw_points",
-                            str_detect(students_col, "max") ~ "max_points",
-                            str_detect(students_col, "time") ~ "submission_time",
-                            str_detect(students_col, "late") ~ "lateness",
-                            TRUE ~ "other"))
-  return (assignments)
+  assignments <- tibble(gs_col = gs_cols) %>%
+    #General regex to rename assignments and add a column "category" in table
+     mutate(gs_col = str_replace_all(tolower(gs_col), "[\\s:-]+", "_"),
+            category = substr(str_replace_all(tolower(gs_col), "[\\s:-]+", "_"), 1, regexpr("_", str_replace_all(tolower(gs_col), "[\\s:-]+", "_")) - 1))%>%
+     #This allows to filter 1 column per assignment in "Assignments Tab"
+     mutate(type = case_when(!str_detect(gs_col, "name|sections|max|time|late")~ "raw_points"))
+ 
+   return (assignments)
 }
