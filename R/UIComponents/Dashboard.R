e7081749ccc9127a0832761b02fd2022197b31ce
Nikita Jayaprakash <nikita@Nikitas-MacBook-Air.local>
2023-04-15 09:45:55 -0700

    small change

--- R/UIComponents/Dashboard.R
+++ R/UIComponents/Dashboard.R
@@ -4,7 +4,7 @@ Dashboard <- tabPanel("Dashboard",
                           plotOutput("assign_dist"),
                           selectInput("which_cat", "Pick a Category", choices = ''),
                           plotOutput("cat_dist"),
-                          plotOutput("letter_dist")
+                          plotOutput("grade_dist")
                           #renderUI("dists")
                          #splitLayout(cellWidths = c("50%", "50%"), plotOutput("plotgraph1"), plotOutput("plotgraph2"))
                        )
